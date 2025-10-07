7f73ff3044d73cb597d0d72705c2a3e4c3bcbc19
Helen Huy <155997163+helenh105@users.noreply.github.com>
2025-10-07 12:22:31 -0700

    updated backend.js and package.json

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,12 +2,69 @@
 import express from "express";
 
 const app = express();
-const port = 8000;
+const port = 8077;
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = { users_list: result};
+        res.send(result);
+    } else {
+        res.send(users);
+    }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
@@ -15,3 +72,7 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
+
+
+

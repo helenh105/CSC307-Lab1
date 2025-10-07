63e4dbf47ce823406221e4bd7a80ef5e8c71e395
Helen Huy <155997163+helenh105@users.noreply.github.com>
2025-10-07 15:04:16 -0700

    added new function fetchUsers, useEffect

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,22 +1,33 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-   const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-   function removeOneCharacter(index) {
+  function removeOneCharacter(index) {
 	const updated = characters.filter((character, i) => {
 	  return i !== index;
 	});
 	setCharacters(updated);
-     }
+  }
 
-   function updateList(person) {
+  function updateList(person) {
 	setCharacters([...characters, person]);
-   }
+  }
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:5173/users");
+    return promise;
+  }
+
+  useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+}, [] );
 
   return (
     <div className="container">
@@ -28,5 +39,8 @@ function MyApp() {
     </div>
   );
 }
+
+
+
 
 export default MyApp;
